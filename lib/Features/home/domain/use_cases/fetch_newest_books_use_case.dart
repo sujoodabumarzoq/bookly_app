58186ea8dc026fb5af7506ee8bcde43c571c778c
Your Name <sjwdabwmrzwq65@gmail.com>
2023-08-21 18:28:59 +0300

    FetchFeaturedNewUsaCase

---
+++ lib/Features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -0,0 +1,19 @@
+import 'package:bookly_app/Features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failur.dart';
+import '../../../../core/use_case/noParam_usecase.dart';
+import '../entites/entities.dart';
+
+class FetchFeaturedNewUsaCase extends UseCase<List<BookEntity>, NoParam> {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedNewUsaCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) {
+    return homeRepo.fetchNewestBooks();
+  }
+}
+
+class NoParam {}
