cf4adaa87c096e3853b696478ec0ee69f632a9ef
Your Name <sjwdabwmrzwq65@gmail.com>
2023-08-26 16:12:48 +0300

    registerAdapter&openBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:bookly_app/Features/home/domain/entites/entities.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
 
 
-void main() {
+void main() async {
   runApp(const Bookly());
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
 }
 
 class Bookly extends StatelessWidget {
