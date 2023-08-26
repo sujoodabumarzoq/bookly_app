44bc51cb2018aa7707716f329a108a8bcd46499a
Your Name <sjwdabwmrzwq65@gmail.com>
2023-08-26 16:23:43 +0300

    cache featured books

--- lib/Features/home/data/data_sources/home_remote_data_sources.dart
+++ lib/Features/home/data/data_sources/home_remote_data_sources.dart
@@ -1,5 +1,7 @@
 import 'package:bookly_app/Features/home/data/model/book_model/item.dart';
 import 'package:bookly_app/Features/home/domain/entites/entities.dart';
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/funcation/save_books.dart';
 import 'package:bookly_app/network/api_base_helper.dart';
 
 abstract class HomeRemoteDataSource {
@@ -11,42 +13,42 @@ abstract class HomeRemoteDataSource {
 // هنا من هدف اني اجلب البيانات مش اعمل اذا هو خطا اولا
 // repo هو الي هيحدد اذا صح او خطا
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
- final ApiBaseHelper apiBaseHelper;
+  final ApiBaseHelper apiBaseHelper;
 
   HomeRemoteDataSourceImpl(this.apiBaseHelper);
-  @override
-  Future<List<BookEntity>> fetchFeaturedBooks()async {
-    var data= await apiBaseHelper.get("volumes?Filtering=free-ebooks&Sorting=newest &q=computer science");
-
-
-        List<BookEntity> books = getBooksList(data);
-        return books ;
-  }
-
 
   @override
-  Future<List<BookEntity>> fetchNewestBooks()async {
-    var data= await apiBaseHelper.get("volumes?Filtering=free-ebooks&q=computer science");
-
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiBaseHelper.get(
+        "volumes?Filtering=free-ebooks&Sorting=newest &q=computer science");
 
     List<BookEntity> books = getBooksList(data);
-    return books ;
+
+    saveBooksData(books, kFeaturedBox);
+
+    return books;
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() async {
+    var data = await apiBaseHelper
+        .get("volumes?Filtering=free-ebooks&q=computer science");
+
+    List<BookEntity> books = getBooksList(data);
+    return books;
   }
 
 // هنا ينفذذ كل المطلوب منو في data remote
 
- List<BookEntity> getBooksList(data) {
-   List<BookEntity> books = [];
-   // مكان الايتم
-   for(var bookmap in data["items"]){
-     // هنا هيبعتلي اكتب
-     // علشان اضيفو موب كتب
-     //هيرجعلي كتب ماب
-     books.add(Item.fromJson(bookmap) );
-   }
-   return books;
- }
-
-
+  List<BookEntity> getBooksList(data) {
+    List<BookEntity> books = [];
+    // مكان الايتم
+    for (var bookmap in data["items"]) {
+      // هنا هيبعتلي اكتب
+      // علشان اضيفو موب كتب
+      //هيرجعلي كتب ماب
+      books.add(Item.fromJson(bookmap));
+    }
+    return books;
+  }
 }
-
