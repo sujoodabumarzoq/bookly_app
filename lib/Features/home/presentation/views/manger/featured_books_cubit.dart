79b1f3deae89a0b4f7e812ad1cefbc7d9b8a1b2d
Your Name <sjwdabwmrzwq65@gmail.com>
2023-08-30 14:18:50 +0300

    implemtn cubit featch featured books

--- lib/Features/home/presentation/views/manger/featured_books_cubit.dart
+++ lib/Features/home/presentation/views/manger/featured_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/domain/entites/entities.dart';
-import 'package:meta/meta.dart';
+import 'package:bookly_app/Features/home/domain/use_cases/fetch_featured_books_usa_case.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
-}
+  FeaturedBooksCubit(this.featuredBooksUseCase) : super(FeaturedBooksInitial());
+// هيتعامل مع UsaCase
+  final FetchFeaturedBooksUsaCase featuredBooksUseCase;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    //هنا بيرجع either صح وغلط
+    var result = await featuredBooksUseCase.call();
+    //fold :  بتاخد 2 طرق
+    //1
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.message));
+      //2
+    }, (books) {
+      emit(FeaturedBooksSucces(books));
+    });
+  }}
