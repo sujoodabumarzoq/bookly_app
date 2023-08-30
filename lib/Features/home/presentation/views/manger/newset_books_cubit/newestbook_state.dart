174c2a8071cc8b98b625daa8b35c326cf09ee7e5
Your Name <sjwdabwmrzwq65@gmail.com>
2023-08-30 14:29:40 +0300

    create newest books cubit

---
+++ lib/Features/home/presentation/views/manger/newset_books_cubit/newestbook_state.dart
@@ -0,0 +1,20 @@
+
+import 'package:bookly_app/Features/home/domain/entites/entities.dart';
+
+abstract class NewestBooksState {}
+
+class NewestBooksInitial extends NewestBooksState {}
+
+class NewestBooksLoading extends NewestBooksState {}
+
+class NewestBooksSuccess extends NewestBooksState {
+  final List<BookEntity> books;
+
+  NewestBooksSuccess(this.books);
+}
+
+class NewestBooksFailure extends NewestBooksState {
+  final String errMessage;
+
+  NewestBooksFailure(this.errMessage);
+}
