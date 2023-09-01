4ba9c7d4f5c1e9376e5410bae54c8ed50f9e40a7
Your Name <sjwdabwmrzwq65@gmail.com>
2023-09-01 12:59:16 +0300

    refinish

--- lib/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xff100B20);
-const kTranstionDuration = Duration(milliseconds: 250);
-const kGtSectraFine = 'GT Sectra Fine';
-const kFeaturedBox = 'featured_box';
-const kNewsBox = 'News_box';
--- lib/core/utils/funcation/save_books.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bookly_app/Features/home/domain/entites/entities.dart';
-import 'package:hive_flutter/adapters.dart';
-
-void saveBooksData(List<BookEntity> books, String boxName ) {
-
-  var box = Hive.box<BookEntity>(boxName);
-  box.addAll(books);
-}
